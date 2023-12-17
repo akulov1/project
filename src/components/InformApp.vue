6c542308d027768ef72ce6f095afdc8f18363b6d
flokxxy <vikiava8@gmail.com>
2023-12-17 15:28:41 +0300

    add block inform

---
+++ src/components/InformApp.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class = "inform-region container-fluid">
+    <div class = "competencies">
+      <div class = "container">
+        <div class = "row">
+          <div class = "col-md-6">
+            <h1 class = "competency-title">13 лет совершенствуем компетенции в Друпал поддержке!</h1>
+            <div class = "competencies-description">Разрабатываем и оптимизируем модули, расширяем функциональность сайтов, обновляем дизайн</div>
+          </div>
+        </div>
+        <div class = "row competencies-row row-flex mt-3 mb-3 ">
+          <div class = "col-6 col-sm-3 ">
+            <div class = "competency mb-3">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/druplar-logo.svg" alt = "druplar-logo" class = "position-absolute logoForOpacity"/>
+                      <img src = "../assets/img/competency-1.svg" alt = "competency-1" class = "img-responsive position-relative" style = "margin-left: 3em"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "competency-body col-9" style = "margin-left: 3em">Добавление информации на сайт, создание новых разделов</div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-md-3">
+            <div class = "competency">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/druplar-logo.svg" alt = "druplar-logo" class = "position-absolute logoForOpacity"/>
+                      <img src = "../assets/img/competency-2.svg" alt = "competency-2" class = "img-responsive position-relative" style = "margin-left: 3em"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "competency-body col-8" style = "margin-left: 3em">Разработка и оптимизация модулей сайта</div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-md-3">
+            <div class = "competency">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/druplar-logo.svg" alt = "druplar-logo" class = "position-absolute logoForOpacity"/>
+                      <img src = "../assets/img/competency-3.svg" alt = "competency-3" class = "img-responsive position-relative" style = "margin-left: 3em"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "competency-body col-9" style = "margin-left: 3em">Интеграция с CRM, 1С, платёжными системами, любыми веб-сервисами</div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-md-3">
+            <div class = "competency">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/druplar-logo.svg" alt = "druplar-logo" class = "position-absolute logoForOpacity"/>
+                      <img src = "../assets/img/competency-4.svg" alt = "competency-4" class = "img-responsive position-relative" style = "margin-left: 3em"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "competency-body col-7" style = "margin-left: 3em">Любые доработки функционала и дизайна</div>
+              </div>
+            </div>
+          </div>
+
+
+          <div class = "col-6 col-md-3">
+            <div class = "competency">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/druplar-logo.svg" alt = "druplar-logo" class = "position-absolute logoForOpacity mt-3"/>
+                      <img src = "../assets/img/competency-5.svg" alt = "competency-5" class = "img-responsive position-relative mt-3" style = "margin-left: 3em"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "competency-body col-7 mt-3 mb-3 " style = "margin-left: 3em">Аудит и мониторинг безопасности Drupal-сайтов</div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-md-3">
+            <div class = "competency">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/druplar-logo.svg" alt = "druplar-logo" class = "position-absolute logoForOpacity mt-3"/>
+                      <img src = "../assets/img/competency-6.svg" alt = "competency-6" class = "img-responsive position-relative mt-3" style = "margin-left: 3em"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "competency-body col-7" style = "margin-left: 3em">Миграция, импорт контента и апгрейд Drupal</div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-md-3">
+            <div class = "competency">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/druplar-logo.svg" alt = "druplar-logo" class = "position-absolute logoForOpacity mt-3 "/>
+                      <img src = "../assets/img/competency-7.svg" alt = "competency-7" class = "img-responsive position-relative mt-3" style = "margin-left: 3em"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "competency-body col-7 " style = "margin-left: 3em">Оптимизация и ускорение Drupal-сайтов</div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-md-3">
+            <div class = "competency">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/druplar-logo.svg" alt = "druplar-logo" class = "position-absolute logoForOpacity mt-3"/>
+                      <img src = "../assets/img/competency-8.svg" alt = "competency-8" class = "img-responsive position-relative mt-3" style = "margin-left: 3em"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "competency-body col-9" style = "margin-left: 3em">Веб-маркетинг, консультации и работы по SEO</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default
+{
+  name: 'InformApp'
+}
+</script>
+
+<style scoped>
+@media (max-width: 1300px)
+{
+  .container
+  {
+    position: relative;
+    z-index: 15;
+  }
+}
+
+.competencies
+{
+  padding: 100px 0;
+}
+
+.competency-title
+{
+  font-size: 42px;
+  font-weight: 1000;
+  margin-bottom: 20px;
+}
+
+.competencies-description
+{
+  margin-bottom: 55px;
+}
+.logoForOpacity{
+  opacity: 0.15;
+  width: 5%;
+  height: 8%;
+
+}
+.competency-body
+{
+  font-size: 15px;
+  margin-top: 20px;
+}
+</style>
