3b3d899cd963310272ee65a0cab176d2b65342db
flokxxy <vikiava8@gmail.com>
2023-12-17 16:46:04 +0300

    changed smth

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
 createApp(App).mount('#app')
