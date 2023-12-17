0eb76494538a2e1e6e285d9131cb4118eb24be66
flokxxy <vikiava8@gmail.com>
2023-12-17 18:47:47 +0300

    change teamApp add

--- src/components/TeamApp.vue
+++ src/components/TeamApp.vue
@@ -16,18 +16,85 @@
                 <div class = "competency-header">
                   <div class = "competency-icon">
                     <div class = "container">
-                      <img src = "../assets/img/IMG_2472_0.jpg" alt = "IMG_2472_0" class = "img-responsive photo"/>
+                      <img src = "../assets/img/IMG_2472_0.jpg" alt = "IMG_2472_0" class = "img-responsive"/>
                     </div>
                   </div>
                 </div>
                 <div class = "container">
                   <h1>Сергей Синица</h1>
-                  <div>Руководитель отдела веб-разработки, канд. тех. наук, заместитель директора</div>
+                  <div class = "competency-body col-sm-8">Руководитель отдела веб-разработки, канд. тех. наук, заместитель директора</div>
                 </div>
               </div>
             </div>
           </div>
-
+          <div class = "col-6 col-sm-4 ots2">
+            <div class = "competency mb-3">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/IMG_2539_0.jpg" alt = "IMG_2539_0" class = "img-responsive"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "container">
+                  <h1>Роман Агабеков</h1>
+                  <div class = "competency-body col-sm-8">Руководитель отдела DevOPS, директор</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-sm-4 ots3">
+            <div class = "competency mb-3">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/IMG_2474_1.jpg" alt = "IMG_2474_1" class = "img-responsive"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "container">
+                  <h1>Алексей Синица</h1>
+                  <div class = "competency-body col-sm-8">Руководитель отдела поддержки сайтов</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-sm-4 ots4">
+            <div class = "competency mb-3">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/IMG_2522_0.jpg" alt = "IMG_2522_0" class = "img-responsive"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "container">
+                  <h1>Дарья Бочкарёва</h1>
+                  <div class = "competency-body col-sm-8">Руководитель отдела продвижения, контекстной рекламы и контент-поддержки сайтов</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class = "col-6 col-sm-4 ots5">
+            <div class = "competency mb-3">
+              <div class = "competency-wrapper">
+                <div class = "competency-header">
+                  <div class = "competency-icon">
+                    <div class = "container">
+                      <img src = "../assets/img/IMG_9971_16.jpg" alt = "IMG_9971_16" class = "img-responsive"/>
+                    </div>
+                  </div>
+                </div>
+                <div class = "container">
+                  <h1>Ирина Торкунова</h1>
+                  <div class = "competency-body col-sm-7">Менеджер по работе с клиентами</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -47,25 +114,83 @@ export default
 }
 </script>
 
-<style>
-h1{
+<style scoped>
+
+@media (max-width: 990px)
+{
+  .ots1, .ots2, .ots3, .ots4, .ots5
+  {
+    margin-left: 200px;
+  }
+}
+
+@media (max-width: 750px)
+{
+  .ots1, .ots2, .ots3, .ots4, .ots5
+  {
+    margin-left: 110px;
+  }
+}
+
+@media (max-width: 590px)
+{
+  .ots1, .ots2, .ots3, .ots4, .ots5
+  {
+    margin-left: 100px;
+  }
+}
+
+@media (max-width: 375px)
+{
+  .ots1, .ots2, .ots3, .ots4, .ots5
+  {
+    margin-left: 10px;
+  }
+}
+
+@media (max-width: 280px)
+{
+  .ots1, .ots2, .ots3, .ots4, .ots5
+  {
+    margin-left: 10px;
+  }
+}
+
+h1
+{
   margin-top: 20px;
   font-size: 20px;
 }
-.photo{
-  height: 300px;
+
+.button_team
+{
+  color: #292b2c;
+  border-color: #292b2c;
+  width: 30%;
+  margin-bottom: 50px;
 }
 
-.team{
+.team
+{
   text-align: center;
   margin-bottom: 50px;
 }
-.button_team{
-  color: #292b2c;
-  border-color: #292b2c;
-  width: 50%;
-  margin-bottom: 50px;
+
+.competencies
+{
+  padding: 150px 0;
 }
 
+.competency-title
+{
+  font-size: 42px;
+  font-weight: 1000;
+  margin-bottom: 20px;
+}
 
+.competency-body
+{
+  font-size: 15px;
+  margin-top: 20px;
+}
 </style>
