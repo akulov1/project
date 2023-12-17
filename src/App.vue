202de13393cce8b49d6935edee94034b784180c9
flokxxy <115942207+flokxxy@users.noreply.github.com>
2023-12-17 15:43:14 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <AppHeader />
+    <InformAdd />
     <!-- Остальное содержимое вашего приложения -->
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
+import InformApp from './components/InformAdd.vue';
 
 export default {
   components: {
     AppHeader,
+    InformAdd,
   },
 };
 </script>
