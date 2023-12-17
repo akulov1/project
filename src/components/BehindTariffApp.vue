345f3d6d773bcacda957e21def1b74dff7e49b2b
flokxxy <vikiava8@gmail.com>
2023-12-17 20:10:28 +0300

    change BehindTariffApp

---
+++ src/components/BehindTariffApp.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="competencies without-padding-bottom" >
+    <div class="container">
+      <!-- ::before -->
+      <div class="row">
+        <div class="row row-flex competencies-row">
+          <div class="col-sm-4 col-xs-12">
+            <div class="competency">
+              <div class="competency-wrapper">
+                <div class="competency-header">
+                  <div class="competency-icon">
+                    <!-- ::before -->
+                    <img src="../assets/img/competency-20.svg" class="img-responsive">
+                  </div>
+                </div>
+                <div class="competency-body">
+                  <div class="competency-time">от 1ч</div>
+                  Настройка события GA
+                  <br>
+                  в интернет-магазине
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 col-xs-12">
+            <div class="competency">
+              <div class="competency-wrapper">
+                <div class="competency-header">
+                  <div class="competency-icon">
+                    <!-- ::before -->
+                    <img src="../assets/img/competency-20.svg" class="img-responsive">
+                  </div>
+                </div>
+                <div class="competency-body">
+                  <div class="competency-time">от 1ч</div>
+                  Настройка события GA
+                  <br>
+                  в интернет-магазине
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
