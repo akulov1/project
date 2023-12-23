ba42a6016055e408cb26d10ad8f98ef724e147e4
flokxxy <vikiava8@gmail.com>
2023-12-23 14:36:47 +0300

    add шрифт

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,7 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
 * {
   font-family: 'Montserrat', sans-serif;
 }
