3dbe930214a1e5c79f506c8834312c578311d45b
flokxxy <vikiava8@gmail.com>
2023-12-25 02:41:34 +0300

    внесено множество изменений

--- src/components/AppReview.vue
+++ src/components/AppReview.vue
@@ -202,7 +202,7 @@ export default {
   rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
   rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
   border-radius: 10px;
-  padding: 30px 40px 30px 40px;
+  padding: 20px 20px 20px 15px;
   margin: 10px 15px 30px 15px;
   display: flex;
   flex-direction: row;
